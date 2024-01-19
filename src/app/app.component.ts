bff1390712cfa6b9a2a0bc02dc6006ec45deeecb
benjamin <benjaminraabe@unomaha.edu>
2024-01-19 21:40:50 +0100

    feat: add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
