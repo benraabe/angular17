a6b34a0d50a7ff0b3fce191e60bd3e9cc68563a8
benjamin <benjaminraabe@unomaha.edu>
2024-01-22 20:18:03 +0100

    feat: fetching data from api with service injecting it

--- src/app/about/company/company.component.ts
+++ src/app/about/company/company.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-company',
@@ -8,6 +10,44 @@ import { RouterLink } from '@angular/router';
   templateUrl: './company.component.html',
   styleUrl: './company.component.scss'
 })
-export class CompanyComponent {
+export class CompanyComponent implements OnInit{
+
+  // http = inject(HttpClient);
+
+  private postService = inject(PostService);
+  posts: any = [];
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  // fetchPost() {
+  //   this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=10').subscribe(response => {
+  //     console.log(response);
+  //     this.posts = response;
+  //   })
+  // }
+
+  // loadPosts() {
+  //   this.postService.getPosts().subscribe(response => {
+  //     console.log(response);
+  //     this.posts = response;
+  //   });
+  // }
+
+
+  loadPosts() {
+    this.postService.getPosts().subscribe({
+      next: (posts: any)  => {
+        this.posts = posts;
+        console.log("Posts: ", posts);
+
+      },
+      error: (err: any) => {
+        console.log("Error: ", err);
+      },
+    });
+  }
+
 
 }
