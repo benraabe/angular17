65aa4ed5dfa945ec739666bfc1b879d3cbfdcf38
benjamin <benjaminraabe@unomaha.edu>
2024-01-23 10:55:37 +0100

    feat implement ngrx counter

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { decrement, increment, reset } from '../counter.action';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  private store = inject(Store);
+  count$?: Observable<number>;
+
+  constructor() {
+    this.count$ = this.store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 
 }
---
+++ src/app/reducers/counter.reducer.ts
@@ -0,0 +1,12 @@
+import { createReducer, on } from "@ngrx/store";
+import { decrement, increment, reset } from "../counter.action";
+
+
+export const inistialState = 0;
+
+export const counterReducer = createReducer(
+  inistialState,
+  on(increment, state => state + 1),
+  on(decrement, state => state - 1),
+  on(reset, state => inistialState)
+);
